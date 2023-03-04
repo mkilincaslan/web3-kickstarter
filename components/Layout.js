345dc3f4f097a1a46b1e788971a524ec2a7e316b
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2023-03-05 01:53:20 +0300

    import Container into Layout to have a beautiful ui

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 import Header from "./Header";
 
 const Layout = ({ children }) => {
 
   return (
-    <div>
+    <Container style={{ paddingTop: '16px' }}>
       <Header />
       {children}
-    </div>
+    </Container>
   );
 };
 
