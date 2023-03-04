ebfd7ebda9f11c4770950d01a76eb5fbb601dbae
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2023-03-04 23:59:50 +0300

    add button to create a campaign

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
 import React from "react";
-import { Card } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-import factory from '../ethereum/factory';
+import { Card, Button } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import factory from "../ethereum/factory";
 
-const CardExampleGroupProps = ({items}) => <Card.Group items={items} />
+const CardExampleGroupProps = ({ items }) => <Card.Group items={items} />;
 
 const Index = ({ contracts }) => {
-  contracts = contracts.map(address => ({ header: address, fluid: true, description: <a>View Campaign</a> }));
+  contracts = contracts.map((address) => ({
+    header: address,
+    fluid: true,
+    description: <a>View Campaign</a>,
+  }));
 
   return (
-    <div><CardExampleGroupProps items={contracts} /></div>
+    <div>
+      <CardExampleGroupProps items={contracts} />
+      <Button content="Create Campaign" icon="add circle" primary labelPosition="right" />
+    </div>
   );
 };
 
 Index.getInitialProps = async (ctx) => {
   const contracts = await factory.methods.getDeployedCampaigns().call();
 
-  return { contracts }
-}
+  return { contracts };
+};
 
 export default Index;
