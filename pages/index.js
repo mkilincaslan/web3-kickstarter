31e39e57b822e4515e582a782f9f1f3d4a5b21fc
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2023-03-07 01:03:36 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Card, Button } from "semantic-ui-react";
-import "semantic-ui-css/semantic.min.css";
 import Layout from "../components/Layout";
 import factory from "../ethereum/factory";
 
