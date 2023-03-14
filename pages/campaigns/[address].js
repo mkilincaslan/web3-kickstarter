11c0dee847e4097e92aa20a7cab2b11a5ac38e2a
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2023-03-15 00:34:24 +0300

    update the format of the cards in detail screen and format of the data

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,21 +1,54 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
+import web3 from "../../ethereum/web3";
 import Layout from "../../components/Layout";
 import { getCampaignByAddress } from "../../ethereum/contracts";
 
 const translateContractDetail = (detail) => {
-  return {
+  const contractObject = {
     minimumContribution: detail[0],
     balance: detail[1],
     requestsCount: detail[2],
     approversCount: detail[3],
     manager: detail[4],
   };
+
+
+  return [
+    {
+      header: 'Minimum Contribution',
+      meta: `${web3.utils.fromWei(contractObject.minimumContribution, 'ether')} eth`,
+      description: 'The minimum amount for the contribution of this particular campaign'
+    },
+    {
+      header: 'Balance',
+      meta: `${web3.utils.fromWei(contractObject.balance, 'ether')} eth`,
+      description: 'The current balance is how much money of this campaign has left to spend'
+    },
+    {
+      header: 'Requests Count',
+      meta: contractObject.requestsCount,
+      description: 'The requests counts of this campaign created by manager to withdraw the money for some costs'
+    },
+    {
+      header: 'Approvers Count',
+      meta: contractObject.approversCount,
+      description: 'The approvers counts of this campaign who contributed/donated to this campaign'
+    },
+    {
+      header: 'Manager',
+      meta: contractObject.manager,
+      description: 'The manager of this campaign who created and manages this campaign',
+      style: { overflowWrap: 'break-word' }
+    }
+  ];
 };
 
 const CampaignDetails = ({ contractDetail }) => {
+
   return (
     <Layout>
-      <h3>Campaign Details</h3>
+      <Card.Group items={contractDetail} />
     </Layout>
   );
 };
