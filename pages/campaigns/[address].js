38f0b22594c7fcd132ea02edb11432f886a5946f
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2023-03-15 00:16:24 +0300

    modify contract detail screen with contract usage and get contract detail

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,12 +1,30 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { getCampaignByAddress } from "../../ethereum/contracts";
 
-const CampaignDetails = () => {
+const translateContractDetail = (detail) => {
+  return {
+    minimumContribution: detail[0],
+    balance: detail[1],
+    requestsCount: detail[2],
+    approversCount: detail[3],
+    manager: detail[4],
+  };
+};
+
+const CampaignDetails = ({ contractDetail }) => {
   return (
     <Layout>
       <h3>Campaign Details</h3>
     </Layout>
   );
+};
+
+CampaignDetails.getInitialProps = async ({ query }) => {
+  const { address } = query;
+  const campaign = getCampaignByAddress(address);
+  const contractDetail = await campaign.methods.getSummary().call();
+  return { contractDetail: translateContractDetail(contractDetail) };
 };
 
 export default CampaignDetails;
