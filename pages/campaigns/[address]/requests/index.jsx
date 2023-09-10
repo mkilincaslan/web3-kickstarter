17765b6891ed3f0200799b2a3880c17a6ab4ea9f
crazycoder.io <mesut@artelax.com>
2023-09-10 13:39:32 +0300

    small ui updates on Requests page

--- pages/campaigns/[address]/requests/index.jsx
+++ pages/campaigns/[address]/requests/index.jsx
@@ -3,31 +3,28 @@ import Link from "next/link";
 import { Button, Grid } from "semantic-ui-react";
 import Layout from "../../../../components/Layout";
 
-const Requests = ({address}) => {
+const Requests = ({ address }) => {
   return (
     <Layout>
       <Grid>
         <Grid.Row>
-          <Grid.Column width={13} />
+          <Grid.Column width={13}>
+            <h2>Requests</h2>
+          </Grid.Column>
           <Grid.Column width={3}>
             <Link href={`/campaigns/${address}/requests/new`}>
-                <Button primary floated="right" content="Add Request" />
+              <Button primary floated="right" content="Add Request" />
             </Link>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row>
-          {/* Table will be here filled by requests data */}
-        </Grid.Row>
+        <Grid.Row>{/* Table will be here filled by requests data */}</Grid.Row>
       </Grid>
     </Layout>
   );
 };
 
 Requests.getInitialProps = async ({ query }) => {
-    const { address } = query;
-    // const campaign = getCampaignByAddress(address);
-    // const contractDetail = await campaign.methods.getSummary().call();
-    return { address };
-  };
+  return { address: query.address };
+};
 
 export default Requests;
